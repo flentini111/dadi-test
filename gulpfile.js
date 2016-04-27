5b1455196844eed461932b5d669703fb149a9df3
Francesco Lentini <francesco.lentini@gmail.com>
2016-04-27 12:49:52 +0000

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@
 const gulp = require('gulp');
 const dust = require('gulp-dust');
 
-gulp.task('default', () => {
+gulp.task('build', () => {
     return gulp.src('templates/*.html')
             .pipe(dust({
                 config: {cjs: true}
             }))
             .pipe(gulp.dest('views'));
 });
+
+// Default task is to build the templates
+gulp.task('default', ['build']);
